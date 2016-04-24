2564647534ef4faee47c23c91bbf058bf668228f
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2016-04-24 17:58:05 +0800

    Add labels seed module

--- server/apis/api.js
+++ server/apis/api.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/posts', require('./post/postRoutes'));
 router.use('/user', require('./user/userRoutes'));
 router.use('/label', require('./label/labelRoutes'));
+router.use('/plan', require('./plan/planRoutes'));
 
 module.exports = router;
--- server/utils/fakeData.js
+++ server/utils/fakeData.js
@@ -43,7 +43,35 @@ const posts = [
   { text: 'There\'s an island in Hujian called Gulang Yu.' }
 ];
 
+const plans = [
+  { startDate: new Date(), endDate: new Date(2016, 9, 2), isArchieved: false },
+  { startDate: new Date(2016, 5, 1), endDate: new Date(2016, 5, 4), isArchieved: false },
+  { startDate: new Date(2016, 5, 2), endDate: new Date(2016, 5, 6), isArchieved: false },
+  { startDate: new Date(2016, 5, 3), endDate: new Date(2016, 5, 8), isArchieved: false },
+  { startDate: new Date(2016, 5, 4), endDate: new Date(2016, 5, 10), isArchieved: false },
+  { startDate: new Date(2016, 5, 5), endDate: new Date(2016, 5, 12), isArchieved: false },
+  { startDate: new Date(2016, 5, 6), endDate: new Date(2016, 5, 14), isArchieved: false },
+  { startDate: new Date(2016, 5, 7), endDate: new Date(2016, 5, 16), isArchieved: false },
+  { startDate: new Date(2016, 5, 8), endDate: new Date(2016, 5, 18), isArchieved: false },
+  { startDate: new Date(2016, 5, 9), endDate: new Date(2016, 5, 22), isArchieved: false },
+  { startDate: new Date(2016, 6, 10), endDate: new Date(2016, 6, 12), isArchieved: false },
+  { startDate: new Date(2016, 6, 11), endDate: new Date(2016, 6, 17), isArchieved: false },
+  { startDate: new Date(2016, 6, 13), endDate: new Date(2016, 6, 20), isArchieved: false },
+  { startDate: new Date(2016, 6, 17), endDate: new Date(2016, 6, 20), isArchieved: false },
+  { startDate: new Date(2016, 6, 12), endDate: new Date(2016, 6, 20), isArchieved: false },
+  { startDate: new Date(2016, 6, 21), endDate: new Date(2016, 6, 25), isArchieved: false },
+  { startDate: new Date(2016, 6, 24), endDate: new Date(2016, 6, 28), isArchieved: false },
+  { startDate: new Date(2016, 6, 27), endDate: new Date(2016, 6, 30), isArchieved: false },
+  { startDate: new Date(2016, 7, 12), endDate: new Date(2016, 8, 2), isArchieved: false },
+  { startDate: new Date(2016, 7, 14), endDate: new Date(2016, 8, 4), isArchieved: false },
+  { startDate: new Date(2016, 7, 16), endDate: new Date(2016, 8, 6), isArchieved: false },
+  { startDate: new Date(2016, 7, 18), endDate: new Date(2016, 8, 8), isArchieved: false },
+  { startDate: new Date(2016, 7, 20), endDate: new Date(2016, 8, 10), isArchieved: false },
+  { startDate: new Date(2016, 7, 22), endDate: new Date(2016, 8, 12), isArchieved: false }
+];
+
 module.exports = {
+  plans,
   posts,
   users,
   cities,
