2ef8b678b5a8952b88b16abd0e039913121eea37
anan-wang13 <anan.wang13@student.xjtlu.edu.cn>
2016-05-19 18:37:26 +0800

    Add user class

---
+++ client/app/src/main/java/app/android/stanfeng/com/hipal/User.java
@@ -0,0 +1,54 @@
+package app.android.stanfeng.com.hipal;
+
+public class User {
+    private final String username;
+    private final String nickname;
+    private final String id;
+    private final String signature;
+    private final String city;
+    private final String gender;
+    private final int age;
+
+    public User (String username, String nickname, String id, String signature, String city, String gender, int age) {
+        this.username = username;
+        this.nickname = nickname;
+        this.id = id;
+        this.signature = signature;
+        this.city = city;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return nickname + " " + age + " " + gender;
+    }
+}
